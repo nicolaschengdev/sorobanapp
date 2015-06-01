3d92f48547f1bd0994c73ac3c94132d0332d911e
WHYERS <ncheng@whyers.com>
2015-06-01 13:16:13 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,275 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>EDF - Sorobanapp</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+	<link rel="stylesheet" href="assets/css/main.css" />
+	<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+
+	<!--[if lt IE 8]>
+	<link rel="stylesheet" href="ie7/ie7.css">
+	<![endif]-->
+</head>
+
+<body>
+	<!-- Header -->
+	<section id="header" class="dark">
+		<header>
+			<img class="logo" src="/images/edf-logo.png">
+			<img class="logo sorobanapp" src="/images/sorobanapp-logo.png">
+
+			<h1>
+				Vos factures d’électricité<br/>
+				ne seront plus une surprise !
+			</h1>
+
+			<!--a class="contact" href="#">NOUS CONTACTER</a-->
+
+			<p> 
+				Application gratuite, prochainement disponible ! 
+			</p>
+		</header>
+
+		<footer>
+			<ul>
+				<li><a href="#"><span class="icon-apple"></span></a></li>
+				<li><a href="#"><span class="icon-android"></span></a></li>
+				<li><a href="#"><span class="icon-windows"></span></a></li>
+			</ul>
+		</footer>
+	</section>
+
+	<!-- First -->
+	<section id="first" class="main">
+
+		<header>
+			<div class="container">
+				<div class="row">
+					<div class="8u 12u(narrow)">
+						<section>
+							<h2>
+								Payez le montant<br/>
+								de votre consommation réelle
+							</h2>
+							<p>
+								Vous êtes un professionnel et vous avez réalisé des <span class="highlight">travaux</span> récemment pour étendre votre local ou votre commerce ?<br/>
+								Vous avez installé de <span class="highlight">nouveaux équipements</span> ? Soyez vigilant, ces changements pourront impacter votre consommation d'électricité.<br/>
+								<br/>
+								Pour éviter toute surprise et rester zen, l’application Soroban vous permet de régler votre <span class="highlight">consommation réelle</span> à chaque facture et de mieux <span class="highlight">anticiper</span> vos dépenses.
+							</p>
+						</section>
+					</div>
+					<div class="4u 12u(narrow)">
+						<section>
+							<img class="iphone" src="images/screen.png" alt="" />
+						</section>
+					</div>
+				</div>
+				
+			</div>
+		</header>
+
+	</section>
+
+	<!-- Second -->
+	<section id="second" class="main">
+		<header>
+			<div class="container">
+				<h2>Comment çà marche ?</h2>
+			</div>
+		</header>
+
+		<div class="content">
+			<div class="container">
+				<div class="row">
+					<picture>
+						<!--[if IE 9]><video style="display: none;"><![endif]-->
+						<source srcset="images/schema-wide.png" media="(min-width: 736px)">
+						<source srcset="images/schema-small.png" media="(min-width: 320px)">
+						<!--[if IE 9]></video><![endif]-->
+						<img src="images/schema-small.png" srcset="schema-small.png" alt="">
+					</picture>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- Third -->
+	<section id="third" class="main">
+		<header>
+			<div class="container">
+				<h2>L’application Soroban assouplit votre gestion</h2>
+			</div>
+		</header>
+
+		<div class="content">
+			<div class="container">
+				
+				<div class="row">
+					<div class="4u 12u(narrow)">
+						<img class="benefit" src="images/benefit_01.png" alt="" />
+
+						<h3>
+							Payez<br/>
+							votre consommation réelle
+						</h3>
+						<p>
+							Fini les surprises en fin de mois.<br/>
+							Vous relevez vous-même votre compteur d'électricité, ainsi vous êtes assuré de payer le prix juste.
+						</p>
+					</div>
+
+					<div class="4u 12u(narrow)">
+						<img class="benefit" src="images/benefit_02.png" alt="" />
+
+						<h3>Bénéficiez de la lecture simplifiée de votre facture</h3>
+						<p>
+							Vous pouvez comparer vos factures<br/>
+							et ainsi mieux analyser votre consommation d'électricité.
+						</p>
+					</div>
+
+					<div class="4u 12u(narrow)">
+						<img class="benefit" src="images/benefit_03.png" alt="" />
+
+						<h3>Envoyez automatiquement vos factures à votre comptable</h3>
+						<p>
+							Gagnez du temps dans votre gestion.<br/>
+							Vos factures d’électricités générées<br/>
+							en ligne sont directement transmises<br/>
+							à votre comptable par mail.
+						</p>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- Fourth -->
+	<section id="fourth" class="main">
+		<div class="content">
+			<div class="container">
+				<div class="row">
+					<div class="6u 12u(narrow)">
+						<img class="conseiller" src="images/conseiller.png" alt="" />
+					</div>
+					<div class="6u 12u(narrow)">
+						<p class="blockquote">
+							Grâce à un relevé réguliier de la consommation réelle, 75% des litiges sont évités...
+							<!--<cite>Philippe D.</cite>-->
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- Five -->
+	<section id="five" class="main">
+		<header>
+			<div class="container">
+				<h2>Inclus dans l'application</h2>
+			</div>
+		</header>
+
+		<div class="content">
+			<div class="container">
+				
+				<div class="row">
+					<div class="4u 12u(narrow)">
+						<img class="feature" src="images/feature_01.png" alt="" />
+
+						<h3>
+							Vous avez une question ?
+						</h3>
+						<p>
+							Demandez à un conseiller de vous rappeler.
+						</p>
+					</div>
+
+					<div class="4u 12u(narrow)">
+						<img class="feature" src="images/feature_02.png" alt="" />
+
+						<h3>Connaître la couleur des jours EJP</h3>
+						<p>
+							ou Tempo si vous avez souscrit à l'une de ces options.
+						</p>
+					</div>
+
+					<div class="4u 12u(narrow)">
+						<img class="feature" src="images/feature_03.png" alt="" />
+
+						<h3>Téléchargez vos factures</h3>
+						<p>
+							mensuelles ou annuelle<br/>
+							(facture regroupée)
+						</p>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+	<!-- Five -->
+	<section id="six" class="main">
+		<header>
+			<div class="container">
+				<h2>Contactez-nous</h2>
+				<p>
+					L’application sera prochainement disponible.<br/>
+					Et bien entendu, elle sera gratuite !
+				</p>
+				<hr>
+				<p class="more">
+					Pour plus d’information, n’hésitez pas à nous contacter.
+				</p>
+				<a class="contact" href="#">NOUS CONTACTER</a>
+			</div>
+		</header>
+
+
+
+	</section>
+
+	<!-- Footer -->
+	<section id="footer">
+		<div class="container">
+			<div class="row">
+				<div class="6u 12u(narrow) imprint">
+					<!--a href="#">Mentions légales</a-->
+				</div>
+				<div class="6u 12u(narrow) copyright">
+					2015 &copy; EDF-Soroban.
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- Scripts -->
+
+	<!--[if lt IE 8]>
+	<script src="ie7/ie7.js"></script>
+	<![endif]-->
+
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/jquery.scrolly.min.js"></script>
+	<script src="assets/js/skel.min.js"></script>
+	<script src="assets/js/util.js"></script>
+	<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+
+	<script>
+	// Picture element HTML5 shiv
+	document.createElement( "picture" );
+	</script>
+	<script src="assets/js/picturefill.min.js" async></script>
+
+	<script src="assets/js/main.js"></script>
+
+</body>
+</html>
